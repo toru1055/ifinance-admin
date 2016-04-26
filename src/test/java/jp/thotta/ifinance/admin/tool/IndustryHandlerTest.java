5ec0c8080862e4270849b1cf629f3b74e1ddce90
toru1055 <toru1055h@gmail.com>
2016-04-26 09:18:18 +0900

    add test persistence file.

---
+++ src/test/java/jp/thotta/ifinance/admin/tool/IndustryHandlerTest.java
@@ -0,0 +1,10 @@
+package jp.thotta.ifinance.admin.tool;
+
+import junit.framework.TestCase;
+
+public class IndustryHandlerTest extends TestCase {
+  public void testIndustry() {
+    String line = "industry list";
+    new IndustryHandler().exec(line.split(" "));
+  }
+}
