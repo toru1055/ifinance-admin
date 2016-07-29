9ac8f9ab8550dfdf4ea89d584daf611f9a27b1c8
Toru HOTTA <toru1055h@gmail.com>
2016-07-29 23:02:46 +0900

    update

--- src/test/java/jp/thotta/ifinance/admin/tool/IndustryHandlerTest.java
+++ src/test/java/jp/thotta/ifinance/admin/tool/IndustryHandlerTest.java
@@ -1,10 +1,11 @@
 package jp.thotta.ifinance.admin.tool;
 
+import jp.thotta.ifinance.common.entity.Industry;
 import junit.framework.TestCase;
 
 public class IndustryHandlerTest extends TestCase {
     public void testIndustry() {
         String line = "industry list";
-        new IndustryHandler().exec(line.split(" "));
+        new MasterDataHandler<Industry>(Industry.class).exec(line.split(" "));
     }
 }
